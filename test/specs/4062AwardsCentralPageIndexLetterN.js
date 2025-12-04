0000b84a46c605174f63e7ac9568a3f15ab33c5a
Rochelle-Cummings <r4cummings2@gmail.com>
2025-12-03 22:58:07 -0700

    Starting of Awards page index items

---
+++ test/specs/4062AwardsCentralPageIndexLetterN.js
@@ -0,0 +1,33 @@
+import { expect, browser } from '@wdio/globals';
+import awardsPage from '../pageobjects/awards.page.js';
+
+describe('AwardsPage N items', () => {
+    it('should navigate through the Awards Central page index letter N items to verify correct links', async () => {
+
+        await browser.url('awards/awards-central/'); 
+        await AwardsPage.nFirstIndexItem.click();
+        await expect(browser).toHaveUrl('https://filestore.scouting.org/filestore/pdf/National_Duty_to_God_Award.pdf');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nSecondIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-honor-patrol/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nThirdIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-major-gift/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nFourthIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-medal-outdoor/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nFifthIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/nesa-life/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nSixthIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/nesa-outstanding/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nSeventhIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/north-star/');
+        await browser.url('awards/awards-central/');
+        await AwardsPage.nEighthIndexItem.click();
+        await expect(browser).toHaveUrl('https://www.scouting.org/expired-content/');
+    })
+
+})
