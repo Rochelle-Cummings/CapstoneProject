58a5c9a9fdb14faaf1e2ea46d5bdb956454c1c61
Rochelle-Cummings <r4cummings2@gmail.com>
2025-12-15 22:53:39 -0700

    Awards Test working and dynamic, etc.

--- test/pageobjects/programs.page.js
+++ test/pageobjects/programs.page.js
@@ -22,21 +22,42 @@ class ProgramsPage extends Page {
         return $('select>option[value="option-14-20"]');
     } 
     //programs page dropdown menus
-    get cubScoutingOpenparagraph() {
-        return $('[data-id="10728773"]');
-    }
-    get scoutsBSAOpenParagraph() {
-        return $('[data-id="5001e34f"]');
-    }
-    get venturingOpenParagraph() {
-        return $('[data-id="4a9aff4d"]');
-    }
-    get seaScoutsOpenParagraph() {
-        return $('[data-id="fe85486"]');
-    }
-    get exploringOpenParagraph() {
-        return $('[data-id="1e7e6211"]');
-    }
+    // get cubScoutingOpenparagraph() {
+    //     return $('[data-id="10728773"]');
+    // }
+    // get scoutsBSAOpenParagraph() {
+    //     return $('[data-id="5001e34f"]');
+    // }
+    // get venturingOpenParagraph() {
+    //     return $('[data-id="4a9aff4d"]');
+    // }
+    // get seaScoutsOpenParagraph() {
+    //     return $('[data-id="fe85486"]');
+    // }
+    // get exploringOpenParagraph() {
+    //     return $('[data-id="1e7e6211"]');
+    // }
+    programsDropDownSelector(name){
+        return $(`//div[contains(text(), "${name}")]`);
+    };
+
+    programPathwayMap=[
+        {Name: "Cub Scouting",
+        OpenedParagSelector: $('[data-id="10728773"]')
+        },
+        {Name: "Scouts BSA",
+        OpenedParagSelector: $('[data-id="5001e34f"]')
+        },
+        {Name: "Venturing",
+        OpenedParagSelector: $('[data-id="4a9aff4d"]')
+        },
+        {Name: "Sea Scouts",
+        OpenedParagSelector: $('[data-id="fe85486"]')
+        },
+        {Name: "Exploring",
+        OpenedParagSelector: $('[data-id="1e7e6211"]')
+        }
+    ]
 
     get cubScoutingDropdown() {
         return $('//div[contains(text(), "Cub Scouting")]');
@@ -52,6 +73,47 @@ class ProgramsPage extends Page {
     }
     get exploringDropdown() {
         return $('//div[contains(text(), "Exploring")]');
+    }
+
+    async openAndCloseTest(){
+        await browser.url('programs/');
+        await ProgramsPage.scoutsBSADropdown.click();
+        await expect(ProgramsPage.scoutsBSAOpenParagraph).toBeDisplayed();
+        await expect(ProgramsPage.cubScoutingOpenparagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.venturingOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.seaScoutsOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.exploringOpenParagraph).not.toBeDisplayed();
+       
+        await ProgramsPage.cubScoutingDropdown.click();
+        await expect(ProgramsPage.cubScoutingOpenparagraph).toBeDisplayed();
+        await expect(ProgramsPage.scoutsBSAOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.venturingOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.seaScoutsOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.exploringOpenParagraph).not.toBeDisplayed();
+
+        await ProgramsPage.venturingDropdown.click();
+        await expect(ProgramsPage.venturingOpenParagraph).toBeDisplayed();
+        await expect(ProgramsPage.cubScoutingOpenparagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.scoutsBSAOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.seaScoutsOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.exploringOpenParagraph).not.toBeDisplayed();
+
+        await ProgramsPage.seaScoutsDropdown.click();
+        await expect(ProgramsPage.seaScoutsOpenParagraph).toBeDisplayed();
+        await expect(ProgramsPage.cubScoutingOpenparagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.scoutsBSAOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.venturingOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.exploringOpenParagraph).not.toBeDisplayed();
+  
+        await ProgramsPage.exploringDropdown.click();
+        await expect(ProgramsPage.exploringOpenParagraph).toBeDisplayed();
+        await expect(ProgramsPage.cubScoutingOpenparagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.scoutsBSAOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.venturingOpenParagraph).not.toBeDisplayed();
+        await expect(ProgramsPage.seaScoutsOpenParagraph).not.toBeDisplayed();
+        await ProgramsPage.cubScoutingDropdown.click();
+        await expect(ProgramsPage.exploringOpenParagraph).not.toBeDisplayed();
+
     }
 
 }
--- test/specs/4062AwardsCentralPageIndexLetterN.js
+++ test/specs/4062AwardsCentralPageIndexLetterN.js
@@ -6,31 +6,6 @@ describe('AwardsPage N items', () => {
         await AwardsPage.dutyToGodAwardTest();
         await AwardsPage.awardNTest();
         await AwardsPage.novaAwardsTest();
-        // await browser.url('awards/awards-central/'); 
-        // await AwardsPage.nFirstIndexItem.click();
-        // const url = await browser.getUrl();
-        // await expect(url).toContain('National_Duty_to_God_Award');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nSecondIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-honor-patrol/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nThirdIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-major-gift/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nFourthIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/national-medal-outdoor/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nFifthIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/nesa-life/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nSixthIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/nesa-outstanding/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nSeventhIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/awards/awards-central/north-star/');
-        // await browser.url('awards/awards-central/');
-        // await AwardsPage.nEighthIndexItem.click();
-        // await expect(browser).toHaveUrl('https://www.scouting.org/expired-content/');
     })
 
 })
